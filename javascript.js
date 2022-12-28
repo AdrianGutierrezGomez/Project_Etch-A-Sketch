374c65d1f92fae7ace096b12038e209736b231f4
Adrian Gutierrez Gomez <adriangtzg.reverie@gmail.com>
2022-12-28 17:42:31 -0600

    Add logic to generate grid

--- javascript.js
+++ javascript.js
@@ -1,8 +1,29 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value + ' x '+ slider.value; // Display the default slider value
+// Display the default slider value
+output.innerHTML = slider.value + ' x '+ slider.value; 
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value + ' x ' + this.value;
 }
+
+let userNumberRows = slider.oninput;
+let userNumberColumns = slider.oninput;
+const clear = document.getElementById("clear");
+const container = document.querySelector(".container");
+
+function createGrid(numberRows,numberColumns){
+    for(let i = 0; i < numberRows; i++){
+        let row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for(let j = 0; j < numberColumns; j++){
+            let column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);        
+        }
+    }
+}
+
+createGrid(16,16)
